f8b7ce3953a1d28ace93020273c8b1be07f160a2
Magdalena Painter <paintermagdalena@gmail.com>
2020-06-09 15:33:51 -0400

    done drill 3

--- src/drill.js
+++ src/drill.js
@@ -8,13 +8,47 @@ const db=knex({
 
 console.log('knex and driver installed correctly');
 
-function knexinfo(searchTerm){
+// function knexinfo(searchTerm){
+//     db
+//         .select('*')
+//         .from('shopping_list')
+//         .where('name', 'ILIKE', `%${searchTerm}%`)
+//         .then(result=>{console.log(result)})
+//         .finally(()=>db.destroy());
+// }
+
+// knexinfo('loaf')
+
+// function paginateItems(pageNumber) {
+//     const itemsPerPage = 6
+//     const offset = itemsPerPage * (pageNumber - 1)
+//     db  
+//         .select('*')
+//         .from('shopping_list')
+//         .limit(itemsPerPage)
+//         .offset(offset)
+//         .then(result => {
+//             console.log(result)
+//         })
+//         .finally(()=>db.destroy());
+// }
+
+// paginateItems(2)
+
+function resentItems(daysAgo) {
     db
-        .select('*')
-        .from('shopping_list')
-        .where('name', 'ILIKE', `%${searchTerm}%`)
-        .then(result=>{console.log(result)})
-        .finally(()=>db.destroy());
+    .select('*')
+    .from('shopping_list')
+    .where(
+        'date_added',
+        '>',
+        db.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+    )
+    .then(result => {
+        console.log(result)
+      })
+    .finally(()=>db.destroy());
 }
 
-knexinfo('loaf')
+resentItems(2)
+
